3c7b0d6d83d10b4bfb03f743805c0cacc98d8d35
Woodlum Hoodlum Davis <woodlumhoodlum>
2024-10-14 21:06:12 -1000

    global any eslint

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-prototype-builtins */
+/* eslint-disable  @typescript-eslint/no-explicit-any */
 import { type ClassValue, clsx } from "clsx";
 import qs from "qs";
 import { twMerge } from "tailwind-merge";
